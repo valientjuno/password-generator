2c6fcebe1508578fb4ae737196acf5b2213f5880
valientjuno <jessedoake91@gmail.com>
2025-08-14 12:11:34 -0600

    js code restacked
    js code reorganized along with correct functions or element properties.

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-document.getElementById("generate").addEventListener("click", () =>{
-    const length = parseInt(document.getElementById("length").value);
-    const includeUpperCase = (document.getElementById("upperCase").value);
+function generatePass(
+  length,
+  includeSpecType,
+  includeUpperCase,
+  includeintegType
+) {
+  const specType = "!@#$%^&*()_+{};~?,./";
+  const integType = "0123456789";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    const includeSpecType = (document.getElementById("specType").value);
-    const includeintegType = (document.getElementById("integType").value);
+  let allChars = lowerCase;
+  if (includeSpecType) allChars += specType;
+  if (includeUpperCase) allChars += upperCase;
+  if (includeintegType) allChars += integType;
 
-};
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+  return password;
+}
 
-s
+document.getElementById("generate").addEventListener("click", () => {
+  const length = parseInt(document.getElementById("length").value);
+  const includeUpperCase = document.getElementById("upperCase").checked;
+  const includeSpecType = document.getElementById("specType").checked;
+  const includeintegType = document.getElementById("integType").checked;
 
-const specType = "!@#$%^&*()_+{};~?,./"; 
-const integType ="0123456789";
-const lowerCase = "abcdefghijklmonpqrstuvwxyz";
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-
-
-function generatePass = [];
-
-
-
-const text = document.getElementById("text");
-
-
-// get length
-let length = parseInt(document.querySelector("length").value);
-
-
-// random index sets
-let randomIndex = Math.floor(Math.random() * charSet.length);
+  const password = generatePass(
+    length,
+    includeSpecType,
+    includeUpperCase,
+    includeintegType
+  );
+  document.getElementById("password").value = password;
+});
