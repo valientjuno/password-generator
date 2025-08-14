faf44243f2438242f849b7038a2f917424691824
valientjuno <jessedoake91@gmail.com>
2025-08-14 11:46:42 -0600

    html checkboxs

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let generatePass = [];
+document.getElementById("generate").addEventListener("click", () =>{
+    const length = parseInt(document.getElementById("length").value);
+    const includeUpperCase = (document.getElementById("upperCase").value);
+
+    const includeSpecType = (document.getElementById("specType").value);
+    const includeintegType = (document.getElementById("integType").value);
+
+};
+
+s
+
+const specType = "!@#$%^&*()_+{};~?,./"; 
+const integType ="0123456789";
+const lowerCase = "abcdefghijklmonpqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+function generatePass = [];
+
+
+
 const text = document.getElementById("text");
-const 
+
 
 // get length
 let length = parseInt(document.querySelector("length").value);
-document.getElementById("length").text;
+
 
 // random index sets
 let randomIndex = Math.floor(Math.random() * charSet.length);
