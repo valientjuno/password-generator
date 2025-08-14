f2abcd504f08f6f726e8fad75cbc7d5bdf485ee3
valientjuno <jessedoake91@gmail.com>
2025-08-14 09:14:13 -0600

    v4

---
+++ script.js
@@ -0,0 +1,10 @@
+let generatePass = [];
+const text = document.getElementById("text");
+const 
+
+// get length
+let length = parseInt(document.querySelector("length").value);
+document.getElementById("length").text;
+
+// random index sets
+let randomIndex = Math.floor(Math.random() * charSet.length);
